96f37a4fe47738e4e37b85fabd0f89ff56747aff
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-09 01:36:29 -0700

    New items can be added to the list now

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	//Add item to top of shopping list if field is not empty
+	$(".add").click(function(){
+		var $item = $("#newItem").val();
+		if($item !== ""){
+			$(".shopping-list").prepend(
+				"<li class='item'>" +
+				"<i class='fa fa-square-o fa-2x'></i>" +
+				"<p class='shopping-item'>" + $item + "</p>" +
+				"<input type='text' class='shopping-item' value='" + $item + "'></input>" +
+				"<i class='fa fa-trash fa-2x trash'></i></li>"
+			);
+			$("#newItem").val("");
+		}
+	});
+});
