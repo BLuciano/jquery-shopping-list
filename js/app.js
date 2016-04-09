263e6f074970df92ccba78dab789329bb4bdf685
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-09 03:28:29 -0700

    Items can be toggled as complete/incomplte

--- js/app.js
+++ js/app.js
@@ -5,23 +5,33 @@ $(document).ready(function(){
 		if($item !== ""){
 			$(".shopping-list").prepend(
 				"<li class='item'>" +
-				"<i class='fa fa-square-o fa-2x'></i>" +
+				"<i class='fa fa-square-o fa-2x status'></i>" +
 				"<p class='shopping-item'>" + $item + "</p>" +
 				"<input type='text' class='shopping-item' value='" + $item + "'></input>" +
 				"<i class='fa fa-trash fa-2x trash'></i></li>"
 			);
 			$("#newItem").val("");
-			$(".trash").click(removeItem);
 		}
 	});
 
-	//Add click event handler to remove item
-	$(".trash").click(function(){
-		removeItem;
-	});
+	$("body").on('click', '.trash', removeItem);
+	$("body").on('click', '.status', toggleChecked);
 });
 
 // remove item from shopping list
 function removeItem(){
 	$(this).parent().remove();
+}
+
+//Toggles checked off state on list items{
+function toggleChecked(){
+	if($(this).hasClass("fa-square-o")){
+		$(this).removeClass("fa-square-o");
+		$(this).addClass("fa-check-square");
+		$(this).siblings('p').addClass("checked");
+	} else{
+		$(this).removeClass("fa-check-square");
+		$(this).addClass("fa-square-o");
+		$(this).siblings('p').removeClass("checked");
+	}
 }
